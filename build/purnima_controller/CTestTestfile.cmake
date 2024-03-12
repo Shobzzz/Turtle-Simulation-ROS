fd39437e0094a277bec6ee34b36a4567849f495d
Shobzzz <shobhitpokhriyal90.com>
2024-03-12 16:10:18 +0900

    Commit

---
+++ build/purnima_controller/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nlab/catkin_ws/src/purnima_controller
+# Build directory: /home/nlab/catkin_ws/build/purnima_controller
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
